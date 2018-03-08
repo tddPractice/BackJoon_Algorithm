5ac1a56857ed608792959003831a45b77ab47143
moonformeli <mgyang95@gmail.com>
2018-03-08 21:48:03 +0900

    Moon's LCS 4 is commit

---
+++ 13711_LCS 4/lcs4.cpp
@@ -0,0 +1,21 @@
+/*
+
+2018 Mar 8, made by Moon
+
+*/
+#include<cstdio>
+#include<algorithm>
+using namespace std;
+const int MXN = 1e5;
+int idx[MXN + 1], dp[MXN], n, x;
+int main() {
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) scanf("%d", &x), idx[x] = i;
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        dp[i] = n;
+        *lower_bound(dp, dp + i, idx[x]) = idx[x];
+    }
+    printf("%d", lower_bound(dp, dp + n, n) - dp);
+    return 0;
+}
