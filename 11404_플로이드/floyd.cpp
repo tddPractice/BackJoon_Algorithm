fac6b7244d604609181ec64ff87cb036ad9f0014
moonformeli <mgyang95@gmail.com>
2018-05-26 20:22:32 +0900

    Moon's 플로이드(재채점) is commit

--- 11404_플로이드/floyd.cpp
+++ 11404_플로이드/floyd.cpp
@@ -2,39 +2,63 @@
 
 2018 Apr 8, made by Moon
 
+Edited
+2018 Mary 26, made by Moon
 */
 #include <cstdio>
 #include <algorithm>
-#define INF 987654321
+
+#define MAX 101
+#define INF 99999999
 using namespace std;
 
-int main(void)
-{
-    int n, m, adj[101][101] = {0};
-    scanf("%d %d", &n, &m);
+int n, m;
+int adj[MAX][MAX];
 
-    for(int i = 1 ; i <= n ; i++)
-        for(int j = 1; j <= n ; j++)
-            if(i != j)
-                adj[i][j] = INF;
+void floyd() {
+	for (int k = 1; k <= n; k++) {
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				adj[i][j] = min(adj[i][j], adj[i][k] + adj[k][j]);
+			}
+		}
+	}
 
-    while(m--)
-    {
-        int u, v, w;
-        scanf("%d %d %d", &u, &v, &w);
-        adj[u][v] = min(adj[u][v] , w);
-    }
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			if (adj[i][j] != INF) {
+				printf("%d ", adj[i][j]);
+			}
+			else {
+				printf("%d ", 0);
+			}
+		}
+		printf("\n");
+	}
+}
 
-    for(int k = 1 ; k <= n ; k++)
-        for(int i = 1 ; i <= n ; i++)
-            for(int j = 1; j <= n ; j++)
-                adj[i][j] = min(adj[i][j], adj[i][k] + adj[k][j]);
+int main() {
+	scanf("%d %d", &n, &m);
 
-    for(int i = 1; i <= n ; i++)
-    {
-        for(int j = 1 ; j <= n; j++)
-            printf("%d ", adj[i][j]);
-        puts("");
-    }
-    return 0;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			if (i == j) {
+				adj[i][j] = 0;
+			}
+			else {
+				adj[i][j] = INF;
+			}
+		}
+	}
+
+	while (m--) {
+		int s, e, w;
+		scanf("%d %d %d", &s, &e, &w);
+
+		adj[s][e] = min(adj[s][e], w);
+	}
+
+	floyd();
+
+	return 0;
 }
